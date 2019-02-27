f8928b670e6f6d58ee09c2f876399d685cd804f9
Phonrob Sawasdee <phonrob.s@up.ac.th>
2019-02-27 14:09:01 +0700

    end

--- lib/pages/account.dart
+++ lib/pages/account.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:test_app1/widgets/main_drawer.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:barcode_scan/barcode_scan.dart';
 
 class AccountPage extends StatelessWidget {
   @override
@@ -8,6 +10,27 @@ class AccountPage extends StatelessWidget {
       drawer: MainDrawer(),
       appBar: AppBar(
         title: Text('Account'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Center(
+            child: QrImage(
+              data: 'http://www.up.ac.th',
+              version: 2,
+              size: 200.0,
+            ),
+          ),
+          Center(
+            child: OutlineButton(
+              child: Text('Scan'),
+              onPressed: () {
+                BarcodeScanner.scan().then((code) {
+                  print(code);
+                });
+              },
+            ),
+          )
+        ],
       ),
     );
   }
